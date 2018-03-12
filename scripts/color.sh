c1a1fd102e4b771f250b2a98d7d65b615a7e776b
Martin Friedrich <martin.friedrich@draglet.com>
2018-03-12 17:38:21 +0100

    major rework

---
+++ scripts/color.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+  /bin/echo -e "
+  \e]P0263238
+  \e]P1ff9800
+  \e]P28bc34a
+  \e]P3ffc107
+  \e]P403a9f4
+  \e]P5e91e63
+  \e]P6009688
+  \e]P7cfd8dc
+  \e]P837474f
+  \e]P9ffa74d
+  \e]PA9ccc65
+  \e]PBffa000
+  \e]PC81d4fa
+  \e]PDad1457
+  \e]PE26a69a
+  \e]PFeceff1
+  "
+  # get rid of artifacts
+  clear
