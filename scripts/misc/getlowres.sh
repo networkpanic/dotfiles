9a70c5ef6803a9395d5b42d1ae27d97f9928c826
Martin Friedrich <martin.friedrich@draglet.com>
2018-03-12 17:59:45 +0100

    major rework

--- scripts/misc/getlowres.sh
+++ scripts/misc/getlowres.sh
@@ -1,5 +1,1 @@
-exiftool -sss -r -csv -imagesize /directory/to/files/*.mp4 > out/to/file/output.txt
-
-find . -type f \( -iname "*.avi" -o -iname "*.mp4" -o -iname "*.mkv" \) -execdir mediainfo {} \; | egrep "(Complete name|Width|Height)"
- find -type f \( -iname "*.avi" -o -iname "*.mp4" -o -iname "*.mkv" \) -exec mediainfo "{}" \; -exec bash -c 'sm=$(mediainfo ${} | grep -v 720) : ; echo $sm' \;
-mediainfo --Inform="Video;Resolution=%Width%x%Height%\nCodec=%CodecID%" movies/10_Cloverfield_Lane_\(2016\)
+exiftool -sss -r -csv -imagesize /mnt/cloud/in/movies/*/{*.avi,*.mp4,*.mkv} | grep -ve 19 -ve 3840 -ve 4k -ve x1080 > lowres.csv
